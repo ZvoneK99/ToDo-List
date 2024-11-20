2e9795be022d7bda405fc2225a05da4e81be3c30
Zvone <zvone.kozull1999@gmail.com>
2024-11-20 20:26:48 +0100

    Styled title with golden text and black border

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>ToDo List</Text>
     </View>
   );
 }
-
+//Stilovi za naslov
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        paddingTop: 50,
+    },
+    title: {
+      fontSize: 30,
+      fontWeight: 'bold',
+      color: "#dec509",
+      textShadowColor: "black", //Obrub
+      textShadowOffset: {width: -1, height: 1}, //Offset za sjenu
+      textShadowRadius: 2, //Raspršenje sjene
+    },
 });
